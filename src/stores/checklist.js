52806bf3472f7877418823d0c381c4490435af39
william8815 <williamhsu88157976@gmail.com>
2026-01-04 20:21:35 +0800

    update : checklist default value

--- src/stores/checklist.js
+++ src/stores/checklist.js
@@ -7,20 +7,32 @@ export const useChecklistStore = defineStore('checklist', {
         id: 'luggage',
         name: '行李物資',
         items: [
-          { id: 1, text: '護照與簽證 (VJW)', completed: false },
-          { id: 2, text: '日幣現金', completed: false },
-          { id: 3, text: '充電器與行動電源', completed: false },
-          { id: 4, text: '換洗衣物與發熱衣', completed: false },
-          { id: 5, text: '個人藥品', completed: false }
+          { id: 1, text: '日幣現金', completed: false },
+          { id: 2, text: '充電線與行動電源', completed: false },
+          { id: 3, text: '換洗衣物 (5套)', completed: false },
+          { id: 4, text: '睡衣 (2套)', completed: false },
+          { id: 5, text: '襪子 (5套)', completed: false },
+          { id: 6, text: '毛帽、厚外套', completed: false },
+          { id: 7, text: '個人藥品', completed: false },
+          { id: 8, text: '護照', completed: false },
+          { id: 9, text: '驗光單', completed: false },
+          { id: 10, text: '雨傘', completed: false },
+          { id: 11, text: '拖鞋', completed: false },
         ]
       },
       {
         id: 'tasks',
         name: '出發前待辦',
         items: [
-          { id: 101, text: '預約 Skyliner 車票', completed: false },
-          { id: 102, text: '購買旅遊保險', completed: false },
-          { id: 103, text: '預下載 Google Maps 離線地圖', completed: false }
+          { id: 101, text: '購買來回機票', completed: false },
+          { id: 102, text: '購買旅平險', completed: false },
+          { id: 103, text: '辦好電信漫遊或 e-SIM', completed: false },
+          { id: 104, text: 'Tabelog 預定 敘敘苑 (晴空塔店)', completed: false },
+          { id: 105, text: 'Tabelog 預定 牛舌的檸檬 (新宿)', completed: false },
+          { id: 106, text: '購買 墨田水族館 票券', completed: false },
+          { id: 107, text: '購買 箱根週遊券 (2日)', completed: false },
+          { id: 108, text: '填寫 Visit Japan Web', completed: false },
+          { id: 109, text: 'PayPay 電子錢包', completed: false }
         ]
       }
     ]
